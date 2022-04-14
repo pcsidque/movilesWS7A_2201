e0464d122d6edcad686207e8b42886117147c873
davquev <ingdavquev@gmail.com>
2022-04-14 07:28:59 -0500

    py Sem 3

---
+++ App_sem03_20220407/app/src/main/java/com/example/app_sem03_20220407/DisplayMessageActivity.kt
@@ -0,0 +1,18 @@
+package com.example.app_sem03_20220407
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESS)
+
+        val textView = findViewById<TextView>(R.id.tvData).apply {
+            text = message
+        }
+    }
+}
---
+++ App_sem03_20220407/app/src/main/java/com/example/app_sem03_20220407/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.app_sem03_20220407
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESS = "com.example.app_sem03_20220407"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View){
+        val editText = findViewById<EditText>(R.id.etEmail)
+        val message = editText.text.toString()
+
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESS, message)
+        }
+        startActivity(intent)
+    }
+}
