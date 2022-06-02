206109e68feb4c3f87a0b17a42b7df4a1c9ae06d
davquev <ingdavquev@gmail.com>
2022-06-02 08:33:33 -0500

    App NameGenerator con ListView v2

--- app_uso_listview/lib/main.dart
+++ app_uso_listview/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //final wordPair = WordPair.random();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: RandomWord()
     );
   }
@@ -25,6 +26,7 @@ class RandomWord extends StatefulWidget {
 class _RandomWordState extends State<RandomWord> {
   final List<WordPair> suggestions = <WordPair>[];
   final TextStyle biggerFont = TextStyle(fontSize: 16);
+  final Set<WordPair> saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +35,9 @@ class _RandomWordState extends State<RandomWord> {
     return Scaffold(
       appBar: AppBar(
         title: Text("** StartUp Name Generator **"),
+        actions: <Widget>[
+          IconButton(onPressed: pushSaved, icon: Icon(Icons.library_books))
+        ],
       ),
       body: buildSuggestions(),
     );
@@ -44,8 +49,8 @@ class _RandomWordState extends State<RandomWord> {
         itemBuilder: (BuildContext context, int i){
           if (i.isOdd){
             return Divider(
-              thickness: 3,
-              color: Colors.black26,
+              thickness: 2,
+              color: Colors.limeAccent,
             );
           }
 
@@ -59,11 +64,56 @@ class _RandomWordState extends State<RandomWord> {
   }
 
   Widget buildRow(WordPair suggestion) {
+    final bool alreadySaved = saved.contains(suggestion);
+
     return ListTile(
       title: Text(
         suggestion.asPascalCase,
         style: biggerFont,
       ),
+      trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null
+      ),
+      onTap: (){
+        setState(() {
+          if (alreadySaved){
+            saved.remove(suggestion);
+          }
+          else{
+            saved.add(suggestion);
+          }
+        });
+      },
+    );
+  }
+
+  void pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          builder: (BuildContext context){
+            final Iterable<ListTile> tiles = saved.map(
+                    (WordPair pair){
+                  return ListTile(
+                    title: Text(
+                      pair.asPascalCase,
+                      style: biggerFont,
+                    ),
+                  );
+                }
+            );
+            final List<Widget> divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Saved favorite Names"),
+              ),
+              body: ListView(
+                  children: divided),
+            );
+          }),
     );
   }
 }
